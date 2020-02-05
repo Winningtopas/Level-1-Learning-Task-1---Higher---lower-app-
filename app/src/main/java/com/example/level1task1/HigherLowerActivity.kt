d17931a8ab5fba9e52c22b8bc256c77360f329a4
Winningtopas <jay07@live.nl>
2020-02-05 18:01:51 +0100

    no message

--- app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
@@ -2,6 +2,7 @@ package com.example.level1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class HigherLowerActivity : AppCompatActivity() {
 
@@ -12,4 +13,19 @@ class HigherLowerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
     }
+
+    private fun initViews() {
+        updateUI()
+        initViews()
+    }
+
+    /**
+     * Update the last throw text and the dice image resource drawable with the current throw.
+     */
+    private fun updateUI() {
+        tvLastThrow.text = getString(R.string.last_throw, lastThrow)
+
+        //,
+    }
+
 }
