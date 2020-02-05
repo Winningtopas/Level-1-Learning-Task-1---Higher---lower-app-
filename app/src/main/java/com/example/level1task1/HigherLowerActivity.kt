bf146722a486b60da35980a19d74cc035eaf924b
Winningtopas <jay07@live.nl>
2020-02-05 17:35:59 +0100

    Design

--- app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class HigherLowerActivity : AppCompatActivity() {
 
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
